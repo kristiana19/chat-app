043d428c9012b7fdd11ef322af502661674b67e4
kristiana19 <marusickristijana@gmail.com>
2026-01-24 11:49:00 +0100

    update profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import experss from "express";
 import authRouts from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = experss();
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(experss.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouts);
 
