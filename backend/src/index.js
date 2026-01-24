a7ff4276ab6897c01bfc67235dbe954cfa2d0ee6
kristiana19 <marusickristijana@gmail.com>
2026-01-24 13:00:17 +0100

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import experss from "express";
 import authRouts from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,7 @@ app.use(experss.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouts);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
